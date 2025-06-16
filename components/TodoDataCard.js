727d3769a2870a894175721558b9aff39be013a0
athulcoder <athul.sin.cos.tan@gmail.com>
2025-06-16 12:45:46 +0530

    time data

--- components/TodoDataCard.js
+++ components/TodoDataCard.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function TodoDataCard({ name, count }) {
   return (
-    <div className="w-[100px] h-[90px] bg-[#f6fdfc] rounded-2xl flex flex-col items-center justify-center shadow-lg  transition-shadow duration-300">
+    <div className="w-[130px] h-[120px] bg-[#e5e5e5] rounded-2xl flex flex-col items-center justify-center shadow-lg  transition-shadow duration-300">
       <span className="text-gray-900 text-lg font-semibold">{count}</span>
-      <span className="text-gray-600 text-sm">{name}</span>
+      <span className="text-gray-600 text-sm text-center">{name}</span>
     </div>
   );
 }
--- components/TodoDataGrid.js
+++ components/TodoDataGrid.js
@@ -8,28 +8,31 @@ function TodoDataGrid() {
 
   const [pendingTodo, setPeningTodo] = useState(0);
   const [completedTodo, setCompletedTodo] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(0);
 
   useEffect(() => {
     let pcount = 0;
     let ccount = 0;
-
+    let rTime = 0;
     todos.map((todo) => {
       if (todo.isDone) {
         ccount++;
       } else {
         pcount++;
+        rTime += todo.timeNeed;
       }
-    });
+    }, todos);
 
     setCompletedTodo(ccount);
     setPeningTodo(pcount);
+    setRemainingTime(rTime);
   });
   return (
     <div className="flex justify-center gap-8   items-center lg:justify-center xl:items-start   my-3">
       <div className="grid grid-cols-2 gap-8  md:flex md:justify-around">
         <TodoDataCard name="Pending" count={pendingTodo} />
         <TodoDataCard name="Completed" count={completedTodo} />
-        <TodoDataCard name="Total" count={pendingTodo + completedTodo} />
+        <TodoDataCard name="Time Needed (hours)" count={remainingTime} />
         <TodoDataCard name="Add new" count="+" />
       </div>
 
