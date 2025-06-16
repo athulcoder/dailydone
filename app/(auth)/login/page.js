514ac421cafcd2f355792661dd571487df150dd1
athulcoder <athul.sin.cos.tan@gmail.com>
2025-06-16 20:39:02 +0530

    load animation

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -2,18 +2,22 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import LoadingAnimation from "@/components/LoadingAnimation";
 
 export default function LoginPage() {
   const router = useRouter();
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
     const res = await fetch("/api/login", {
       method: "POST",
@@ -68,12 +72,16 @@ export default function LoginPage() {
           />
         </div>
 
-        <button
-          type="submit"
-          className="w-full py-3 text-white font-semibold rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 transition-all"
-        >
-          Login
-        </button>
+        {loading ? (
+          <LoadingAnimation />
+        ) : (
+          <button
+            type="submit"
+            className="w-full py-3 text-white font-semibold rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 transition-all"
+          >
+            Login
+          </button>
+        )}
       </form>
     </div>
   );
